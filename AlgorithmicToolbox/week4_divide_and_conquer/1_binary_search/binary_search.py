fd950bca098213b15c6fb788708056c274101a2a
Marcel Opprecht <marcel.opprecht@pnp-software.com>
2018-11-15 18:52:26 +0100

    Algo, week4, ex 2

--- AlgorithmicToolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ AlgorithmicToolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,9 +1,24 @@
 # Uses python3
 import sys
 
+def binary_search_range(a, left, right, x):
+
+    if left > right:
+        return -1
+
+    mid = int((right+left)/2)
+
+    if a[mid] > x:
+        return binary_search_range(a, left, mid-1, x)
+    elif a[mid] < x:
+        return binary_search_range(a, mid+1, right, x)
+    else:
+        return mid
+
 def binary_search(a, x):
     left, right = 0, len(a)
-    # write your code here
+
+    return binary_search_range(a, left, right-1, x)    
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -12,11 +27,13 @@ def linear_search(a, x):
     return -1
 
 if __name__ == '__main__':
+    input = "5 1 5 8 12 13\n6 8 1 23 1 12 0"
     input = sys.stdin.read()
+
     data = list(map(int, input.split()))
     n = data[0]
     m = data[n + 1]
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
--- AlgorithmicToolbox/week4_divide_and_conquer/2_majority_element/majority_element.py
+++ AlgorithmicToolbox/week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,18 +1,51 @@
 # Uses python3
 import sys
 
+def count_elem(a, left, right, x):
+    count = 0
+    for i in range(left, right+1):
+        if a[i] == x:
+            count += 1
+    return count
+
 def get_majority_element(a, left, right):
     if left == right:
-        return -1
-    if left + 1 == right:
+        # In case of single leaf, return the element itself as it is the major
         return a[left]
-    #write your code here
+    if left + 1 == right:
+        # In case of only two left, check whether they are equal
+        if a[left] == a[right]:
+            return a[left]
+        else:
+            return -1
+
+    # Combine
+    mid = int((right+left)/2)
+
+    major_left = get_majority_element(a, left, mid)
+    if major_left != -1:
+        # We might have a solution. Check wheter major_left is still a major on this level
+        count = count_elem(a, left, right, major_left)
+        if count > (right-left+1)/2:
+            return major_left
+    
+    major_right = get_majority_element(a, mid+1, right)
+    if major_right != -1:
+        count = count_elem(a, left, right, major_right)
+        if count > (right-left+1)/2:
+            return major_right
+
     return -1
 
 if __name__ == '__main__':
+    input = "5\n2 3 9 2 2"
+    input = "4\n1 2 3 4"
+    input = "4\n1 2 3 1"
+    input = "8\n5 5 5 5 1 1 1 5"
+    input = "10\n512766168 717383758 5 126144732 5 573799007 5 5 5 405079772"
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element(a, 0, n-1) != -1:
         print(1)
     else:
         print(0)
