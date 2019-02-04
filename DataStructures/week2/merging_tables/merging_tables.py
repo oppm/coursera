cd8b01a7ab544d3d66a2f539a2fcfe934e1b7756
Marcel Opprecht <marcel.opprecht@scs.ch>
2019-02-04 19:16:21 +0100

    First try of merging_tables

--- DataStructures/week2/merging_tables/merging_tables.py
+++ DataStructures/week2/merging_tables/merging_tables.py
@@ -2,18 +2,12 @@
 
 import sys
 
-n, m = map(int, sys.stdin.readline().split())
-lines = list(map(int, sys.stdin.readline().split()))
-rank = [1] * n
-parent = list(range(0, n))
-ans = max(lines)
-
-def getParent(table):
+def getParent(parent, i):
     # find parent and compress path
-    return parent[table]
+    return parent[i]
 
-def merge(destination, source):
-    realDestination, realSource = getParent(destination), getParent(source)
+def merge(parent, destination, source):
+    realDestination, realSource = getParent(parent, destination), getParent(parent, source)
 
     if realDestination == realSource:
         return False
@@ -21,11 +15,54 @@ def merge(destination, source):
     # merge two components
     # use union by rank heuristic 
     # update ans with the new maximum table size
-    
+
     return True
 
-for i in range(m):
-    destination, source = map(int, sys.stdin.readline().split())
-    merge(destination - 1, source - 1)
-    print(ans)
+def calc(n, merge_queries, n_rows):
+    parent = list(range(0, n))
+    m = len(merge_queries)
+    outp = []
+
+    for i in range(m):
+        [destination, source] = merge_queries[i]
+        merge(parent, destination - 1, source - 1)
+        outp.append(max(n_rows))
+
+    return outp
+
+def compare(a, b):
+    if len(a) == len(b):
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                return False
+        return True
+    else:
+        return False        
+
+def test(n_tables, n_rows, merge_queries, exp_output):
+    ans = calc(n_tables, merge_queries, n_rows)
+    print("passed" if compare(ans, exp_output) else "failed")        
+
+def unit_test():
+    test(5, [1, 1, 1, 1, 1], [[3, 5], [2, 4], [1, 4], [5, 4], [5, 3]], [2, 2, 3, 5, 5])
+    test(6, [10, 0, 5, 0, 3, 3], [[6, 6], [6, 5], [5, 4], [4, 3]], [10, 10, 10, 11])
+
+def main():
+    n, m = map(int, sys.stdin.readline().split())
+    n_rows = list(map(int, sys.stdin.readline().split()))
+    rank = [1] * n
+    merge_queries = []
+    for i in range(m):
+        destination, source = map(int, sys.stdin.readline().split())
+        merge_queries.append([destination, source])
+
+    ans = calc(merge_queries, n_rows)
     
+    for i in range(m):
+        print(ans)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        unit_test()
+    else:
+        main()  
