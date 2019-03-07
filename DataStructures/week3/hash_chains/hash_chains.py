1aa17ff3a608f0cbccf85452dd409948bfd7df4f
Marcel Opprecht <marcel.opprecht@pnp-software.com>
2019-03-07 19:30:59 +0100

    Adding datastructures week6

--- DataStructures/week3/hash_chains/hash_chains.py
+++ DataStructures/week3/hash_chains/hash_chains.py
@@ -1,4 +1,5 @@
 # python3
+import sys
 
 class Query:
 
@@ -18,6 +19,7 @@ class QueryProcessor:
         self.bucket_count = bucket_count
         # store all strings in one list
         self.elems = []
+        self.buckets = [None] * bucket_count
 
     def _hash_func(self, s):
         ans = 0
@@ -35,6 +37,34 @@ class QueryProcessor:
         return Query(input().split())
 
     def process_query(self, query):
+        if query.type == "check":
+            chain = self.buckets[query.ind]
+            if chain != None:
+                self.write_chain(chain)
+            else:
+                print("")
+        else:
+            hash_val = self._hash_func(query.s)
+            chain = self.buckets[hash_val]
+            if chain == None:
+                chain = []
+
+            try:
+                ind = chain.index(query.s)
+            except ValueError:
+                ind = -1
+
+            if query.type == 'add':
+                if ind == -1:
+                    chain.insert(0, query.s)
+                    self.buckets[hash_val] = chain                    
+            elif query.type == 'find':
+                self.write_search_result(ind != -1)
+            else: # del
+                if ind != -1:
+                    chain.pop(ind)
+
+    def process_query_naive(self, query):
         if query.type == "check":
             # use reverse order, because we append strings to the end
             self.write_chain(cur for cur in reversed(self.elems)
@@ -58,7 +88,61 @@ class QueryProcessor:
         for i in range(n):
             self.process_query(self.read_query())
 
-if __name__ == '__main__':
+def test(bucket_count, queries):
+    proc = QueryProcessor(bucket_count)
+    for q in queries:
+        proc.process_query(q)
+    return True
+
+def test_verify(bucket_count, queries):
+    print("passed" if test(bucket_count, queries) else "failed")
+
+def unit_test():
+#    test(5, [
+#        Query(["add", "world"]),
+#        Query(["add", "HellO"]),
+#        Query(["check", 4]),
+#        Query(["find", "World"]),
+#        Query(["find", "world"]),
+#        Query(["del", "world"]),
+#        Query(["check", 4]),
+#        Query(["del", "HellO"]),
+#        Query(["add", "luck"]),
+#        Query(["add", "GooD"]),
+#        Query(["check", 2]),
+#        Query(["del", "good"])])
+#    test(4, [
+#        Query(["add", "test"]),
+#        Query(["add", "test"]),
+#        Query(["find", "test"]),
+#        Query(["del", "test"]),
+#        Query(["find", "test"]),
+#        Query(["find", "Test"]),
+#        Query(["add", "Test"]),
+#        Query(["find", "Test"])
+#    ])
+    test(3, [
+        Query(["check", 0]),
+        Query(["find", "help"]),
+        Query(["add", "help"]),
+        Query(["add", "del"]),
+        Query(["add", "add"]),
+        Query(["find", "add"]),
+        Query(["find", "del"]),
+        Query(["del", "del"]),
+        Query(["find", "del"]),
+        Query(["check", 0]),
+        Query(["check", 1]),
+        Query(["check", 2])
+    ])
+
+def main():
     bucket_count = int(input())
     proc = QueryProcessor(bucket_count)
     proc.process_queries()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        unit_test()
+    else:
+        main()      
--- DataStructures/week3/hash_substring/hash_substring.py
+++ DataStructures/week3/hash_substring/hash_substring.py
@@ -1,4 +1,5 @@
 # python3
+import sys
 
 def read_input():
     return (input().rstrip(), input().rstrip())
@@ -6,13 +7,80 @@ def read_input():
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
+a_base = 99839
+p = 999961
+
+def _hash_first(pattern):
+    h = 0
+    x = [0] * len(pattern)
+    x_i = 0
+    a = 1
+    for s_i, s in enumerate(pattern):
+        h += (ord(s) * a) % p
+        #print("{0}: {1} * {2}".format(s_i, ord(s), a))
+        x[x_i] = a
+        a = (a * a_base)
+        x_i = (x_i+1) % len(pattern)
+    return [h, x]
+
+def _hash_next(h, s_0, s_n, x, x_i, i):
+    x_prev = (x_i + len(x) - 1) % len(x)
+    x_next = (x_i + 1) % len(x)
+    a = (x[x_prev] * a_base)
+
+    h -= (ord(s_0) * x[x_i])
+    h += (ord(s_n) * a)
+    h %= p
+
+    #print("{0}-: {1} * {2}".format(i, ord(s_0), x[x_i]))
+    #print("{0}+: {1} * {2}".format(i, ord(s_n), a))
+
+    #assert(x[x_i] == a_base**(i-1))
+    #assert(a == a_base**(i+len(x)-1))
+
+    x[x_i] = a
+
+    return [h, x, x_next]
+
 def get_occurrences(pattern, text):
+    [h_p, _] = _hash_first(pattern)
+    res = []
+    x_i = 0
+    for i in range(len(text) - len(pattern) + 1):
+        if i == 0:
+            [h_t, x] = _hash_first(text[0:len(pattern)])
+        else:
+            [h_t, x, x_i] = _hash_next(h_t, text[i-1], text[i+len(pattern)-1], x, x_i, i)
+
+        if h_p == h_t:
+            if pattern == text[i:len(pattern)+i]:
+                res.append(i)
+        h_p = (h_p*a_base) % p
+    return res
+
+def get_occurrences_naive(pattern, text):
     return [
         i 
         for i in range(len(text) - len(pattern) + 1) 
         if text[i:i + len(pattern)] == pattern
-    ]
+    ]    
 
-if __name__ == '__main__':
+def test(t, p):
+    res_a = get_occurrences(p, t)
+    res_b = get_occurrences_naive(p, t)
+    print("passed" if (res_a == res_b) else "failed")
+
+def unit_test():
+    test("abacaba", "aba")
+    test("Test", "testTesttesT")
+    test("aaaaa", "baaaaaaa")
+    test("aaa", "a")
+
+def main():
     print_occurrences(get_occurrences(*read_input()))
 
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        unit_test()
+    else:
+        main()
